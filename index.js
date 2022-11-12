b220c69036fffa2f74e246534fcaf6cab51f85c0
Wallace Leandro <wallaceleandro2003@gmail.com>
2022-11-12 14:43:26 -0200

    test-scroll-behavior

---
+++ index.js
@@ -0,0 +1,9 @@
+function abrir() {
+    const paragrafo = document.getElementById("paragrafo")
+
+    if (paragrafo.style.display === 'none') {
+        console.log('teste')
+    }
+
+    return console.log("Hello world")
+}
